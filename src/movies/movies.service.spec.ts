aa4eaa3c608dac2a9c897291b3fad4bd08937092
Heochan <heochan01@korea.ac.kr>
2024-11-27 02:36:01 +0900

    Test: add 'getAll' and 'getOne' function unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -20,5 +21,35 @@ describe('MoviesService', () => {
   // npm run test:xxx 명령어로 테스트 실행
   it('should be 4', () => {
     expect(2 + 2).toEqual(4);
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      // test에 앞서 먼저 데이터부터 생성해 둔다.
+      service.create({
+        title: 'Test Movie',
+        year: 2024,
+        genres: ['test'],
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
   });
 });
